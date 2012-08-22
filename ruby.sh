06286eece7799988f01ac3f781ab6e11c8fa908c
Marius Ducea <marius.ducea@gmail.com>
2012-08-22 14:38:02 -0700

    chdir first before downloading the rubygems file

--- ruby.sh
+++ ruby.sh
@@ -1,7 +1,7 @@
 yum install -y ruby.x86_64 ruby-devel.x86_64 gcc.x86_64 gcc-c++.x86_64 automake autoconf make.x86_6
 
-wget http://production.cf.rubygems.org/rubygems/rubygems-1.6.2.tgz
 cd /tmp
+wget http://production.cf.rubygems.org/rubygems/rubygems-1.6.2.tgz
 tar zxf rubygems-1.6.2.tgz
 cd rubygems-1.6.2
 ruby setup.rb --no-format-executable
